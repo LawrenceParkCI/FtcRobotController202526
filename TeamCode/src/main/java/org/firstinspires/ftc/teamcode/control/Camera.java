676462df84c7d4e77547db4d67598b5e1a6d17d6
Kemp <christina.kemp@tdsb.on.ca>
2026-02-18 11:31:43 -0500

    added Camera class, refactored RedShoot3Ball

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/Camera.java
@@ -0,0 +1,104 @@
+package org.firstinspires.ftc.teamcode.control;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Camera {
+
+
+    int cameraPosittionTolerance = 5;
+    // Vision
+    private VisionPortal visionPortal;
+    private AprilTagProcessor aprilTag;
+    // Data structures
+    private final Map<Integer, Double> idToDistanceMeters = new HashMap<>();
+    private final List<Integer> seenTagIds = new ArrayList<>();
+
+    public Camera(HardwareMap hardwareMap){
+        AprilTagProcessor.Builder tagBuilder = new AprilTagProcessor.Builder();
+        aprilTag = tagBuilder.build();
+
+        WebcamName webcam = hardwareMap.get(WebcamName.class, "Webcam 1");
+        VisionPortal.Builder portalBuilder = new VisionPortal.Builder()
+                .setCamera(webcam)
+                .addProcessor(aprilTag);
+
+        visionPortal = portalBuilder.build();
+        visionPortal.resumeStreaming();
+    }
+    public boolean isFacingTag(int reqID){
+        List<AprilTagDetection> detections = aprilTag.getDetections();
+        for (AprilTagDetection det : detections) {
+            double position = det.ftcPose.x;
+            if(det.id == reqID && position > -cameraPosittionTolerance && position < cameraPosittionTolerance) {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+    /**
+     * Gives the distance to the required ID tag. Returns -1 if the
+     * specified April Tag is not found.
+     * @param reqID
+     * @return
+     */
+    public double getDistance(int reqID){
+        List<AprilTagDetection> detections = aprilTag.getDetections();
+        for (AprilTagDetection det : detections) {
+            if(det.id == reqID)
+                return det.ftcPose.range;
+        }
+            return -1;
+    }
+
+    public char[]  getPattern() {
+        List<AprilTagDetection> detections = aprilTag.getDetections();
+        for (AprilTagDetection det : detections) {
+            int id = det.id;
+            double distanceMeters = det.ftcPose.range;
+
+            if (id == 21) {
+                return new char[]{'g', 'p', 'p'};
+            } else if (id == 22) {
+                return new char[]{'p', 'g', 'p'};
+            } else if (id == 23) {
+                return new char[]{'p', 'p', 'g'};
+            }
+            // ID 20 = blue alliance scoring, ID 24 = red alliance scoring
+        }
+        return null;
+    }
+
+    public void  updateAprilTagData() {
+        List<AprilTagDetection> detections = aprilTag.getDetections();
+        seenTagIds.clear();
+        idToDistanceMeters.clear();
+        for (AprilTagDetection det : detections) {
+            int id = det.id;
+            double distanceMeters = det.ftcPose.range;
+            idToDistanceMeters.put(id, distanceMeters);
+            if (!seenTagIds.contains(id)) {
+                seenTagIds.add(id);
+            }
+        }
+
+    }
+
+    public void shutdownVision() {
+        if (visionPortal != null) {
+            visionPortal.stopStreaming();
+        }
+    }
+}
